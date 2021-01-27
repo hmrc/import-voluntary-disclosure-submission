f3c25b4bde5a2b1b8259620fbe4eb92bf1844fd5
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-01-27 11:05:00 +0000

    added mockhttp

---
+++ test/mocks/services/MockHttp.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mocks.services
+
+import base.SpecBase
+import org.scalamock.scalatest.MockFactory
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpReads}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait MockHttp extends SpecBase with MockFactory {
+
+  val mockHttp: HttpClient = mock[HttpClient]
+
+  def setupMockHttpGet[T](url: String)(response: T): Unit =
+    (mockHttp.GET[T](_: String)(_: HttpReads[T], _: HeaderCarrier, _: ExecutionContext))
+      .expects(*, *, *, *)
+      .returns(Future.successful(response))
+
+}
