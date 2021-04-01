1474327b91f9900313a75c1ca8a7d142eef5bd1e
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2021-04-01 11:52:48 +0100

    remove old duty types

--- app/models/DutyTypes.scala
+++ app/models/DutyTypes.scala
@@ -26,14 +26,11 @@ object DutyTypes extends Enumeration {
   val A35: DutyType = Value("A35")
   val A20: DutyType = Value("A20")
   val D10: DutyType = Value("D10")
-  val ImportVat: DutyType = Value("importVat") // TODO - remove when feature switch is taken out
   val B00: DutyType = Value("B00")
-  val ExciseDuty: DutyType = Value("exciseDuty") // TODO - remove when feature switch is taken out
   val E00: DutyType = Value("E00")
   val A40: DutyType = Value("A40")
   val A45: DutyType = Value("A45")
   val A10: DutyType = Value("A10")
-  val CustomsDuty: DutyType = Value("customsDuty") // TODO - remove when feature switch is taken out
   val A00: DutyType = Value("A00")
 
   implicit val reads: Reads[DutyType] = Reads.enumNameReads(this)
@@ -43,14 +40,11 @@ object DutyTypes extends Enumeration {
     case A35 => JsString("A35")
     case A20 => JsString("A20")
     case D10 => JsString("D10")
-    case ImportVat => JsString("B00") // TODO - remove when feature switch is taken out
     case B00 => JsString("B00")
-    case ExciseDuty => JsString("E00") // TODO - remove when feature switch is taken out
     case E00 => JsString("E00")
     case A40 => JsString("A40")
     case A45 => JsString("A45")
     case A10 => JsString("A10")
-    case CustomsDuty => JsString("A00") // TODO - remove when feature switch is taken out
     case A00 => JsString("A00")
   }
 }
