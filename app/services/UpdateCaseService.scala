3a2349b3bc9b7eb8c0f75209713ea81cd9fb42b5
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-07-12 16:04:31 +0100

    CIVDT-613: Update case service

---
+++ app/services/UpdateCaseService.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import connectors.EisConnector
+import models.responses.UpdateCaseResponse
+import models.{ErrorModel, UpdateCase}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class UpdateCaseService @Inject()(connector: EisConnector,
+                                  fileTransferService: FileTransferService) {
+
+  def updateCase(updateCase: UpdateCase)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Either[ErrorModel, UpdateCaseResponse]] = {
+    connector.updateCase(updateCase) map {
+      case success@Right(details) =>
+        fileTransferService.transferFiles(details.id, details.correlationId, updateCase.supportingDocuments)
+        success
+      case failure => failure
+    }
+  }
+
+}
---
+++ test/services/UpdateCaseServiceSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import base.ServiceSpecBase
+import data.SampleData
+import mocks.connectors.MockEisConnector
+import mocks.services.MockFileTransferService
+import models.responses.UpdateCaseResponse
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
+
+import java.util.UUID
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class UpdateCaseServiceSpec
+  extends ServiceSpecBase
+    with MockFileTransferService
+    with MockEisConnector
+    with SampleData {
+
+  "updateCase" should {
+
+    "return whatever the connector returns" in {
+      val expectedResponse = Right(UpdateCaseResponse("some id", UUID.randomUUID().toString))
+      MockedEisConnector.updateCase(updateCase, expectedResponse)
+      MockedFileTransferService.transferFiles()
+      val service = new UpdateCaseService(mockEisConnector, mockFileTransferService)
+      await(service.updateCase(updateCase)) mustBe expectedResponse
+    }
+  }
+}
