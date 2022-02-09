de403d1cdcbb1c5d3dff2c4ac1a49739adc8bfbf
jpatton638 <jpatton638@users.noreply.github.com>
2022-02-09 11:39:55 +0000

    DDCYLS-427 | Library upgrades for backend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % "5.18.0",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.58.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.13.0"
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % "5.20.0",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"        % "0.59.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.13.1"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.18.0" % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.20.0" % Test,
     "org.scalatest"          %% "scalatest"              % "3.2.10" % "test, it",
     "org.pegdown"             % "pegdown"                % "1.6.0"  % "test, it",
     "com.typesafe.play"      %% "play-test"              % current  % Test,
@@ -18,6 +18,6 @@ object AppDependencies {
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.62.2" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"  % "test, it",
     "org.jsoup"               % "jsoup"                  % "1.14.3" % Test,
-    "com.github.tomakehurst"  % "wiremock-jre8"          % "2.31.0" % "test, it"
+    "com.github.tomakehurst"  % "wiremock-jre8"          % "2.32.0" % "test, it"
   )
 }
